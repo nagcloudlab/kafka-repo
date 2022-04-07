eb88e1faacdabce65e7e3e3cffc7149f38884917
nag <nagtraininglab@gmail.com>
2022-04-07 13:30:06 +0530

    final

---
+++ Kafka-programming/kafka-spring-invoice-producer/practice-resources/kafka-scripts.sh
@@ -0,0 +1,7 @@
+
+
+
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic pos-topic
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic pos-topic --from-beginning --property print.key=true --property key.separator=":"
+
---
+++ Kafka-programming/kafka-spring-invoice-streams/src/test/java/com/example/jsonposfanout/JsonposfanoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonposfanout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonposfanoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
